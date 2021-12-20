aec230e19a1a75748ca77f8cbf1ea59b7e4754a9
AK <aruncsengr@gmail.com>
2021-12-20 07:17:58 +0530

    feat: saving work duration upon tasks done

--- api/app/models/task_session.rb
+++ api/app/models/task_session.rb
@@ -1,4 +1,14 @@
 class TaskSession < ApplicationRecord
   belongs_to :task
   belongs_to :user
+
+  validates :start_time, :end_time, :duration, presence: true
+
+  before_save :set_duration
+
+  private
+
+  def set_duration
+    self.duration = (end_time - start_time)
+  end
 end
