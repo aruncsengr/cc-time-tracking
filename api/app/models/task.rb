5cccef498138b7140e4fc01663604bf71c0d13b5
AK <aruncsengr@gmail.com>
2021-12-20 07:20:10 +0530

    feat: added callback to save session with user work progress

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,5 +1,18 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :task_sessions
 
   validates :answer, presence: true, if: :submitted?
+
+  after_save :create_task_session
+
+  private
+
+  def create_task_session
+    task_sessions.create!(
+      user_id: user_id,
+      start_time: start_time,
+      end_time: end_time
+    )
+  end
 end
