8f37250b5292f73b826b7812cc5c9a10b6a7dab3
AK <aruncsengr@gmail.com>
2021-12-20 08:05:27 +0530

    feat: statistics using service object desgin pattern   - Code refactored to avoid multiple DB query hits   - Taskwise total duration, avg duration , session count computation added   - User total duration, avg duration, avg duration to submit task   - Integrated wih user and task APIs

---
+++ api/app/services/statistical_service.rb
@@ -0,0 +1,45 @@
+class StatisticalService
+  METRIC_UNIT = "seconds".freeze
+
+  attr_reader :obj
+
+  def initialize(obj)
+    @obj = obj
+  end
+
+  def task_statistics_specific
+    collection = obj.task_sessions.submitted
+    compute_statistics(collection)
+  end
+
+  def task_statistics_overall
+    collection = obj.task_sessions
+    stats = compute_statistics(collection)
+    stats[:average_duration_to_submit] = collection.submitted.average(:duration)
+    stats
+  end
+
+  private
+
+  def compute_statistics(collection)
+    arel_table = TaskSession.arel_table
+    stats = collection.pluck(
+      arel_table[:duration].sum,
+      arel_table[:duration].average,
+      arel_table[:id].count
+    ).first
+
+    stats_with_units(stats)
+  end
+
+  def stats_with_units(stats)
+    total_duration, average_duration, session_count = stats
+
+    {
+      total_duration: total_duration,
+      average_duration: average_duration,
+      session_count: session_count,
+      metric_unit: METRIC_UNIT
+    }
+  end
+end
